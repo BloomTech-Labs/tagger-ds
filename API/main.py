8da78b89798dd716cf51eaa7bebf7a569541b570
SamH3pn3r <samuelhepner03@gmail.com>
2019-12-13 12:19:54 -0800

    updates

--- API/main.py
+++ API/main.py
@@ -1,5 +1,7 @@
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize.regexp import regexp_tokenize
+import numpy as np
+
 
 
 # A function that removes all unnecessary puncuation, html code, and/or any apostrophes lying around
@@ -19,8 +21,23 @@ def clean_text(text):
     return text
 
 class tokenize:
-    def regnltk_tokenize(text):
+    def regnltk_tokenize(self, text):
         lemmatizer = WordNetLemmatizer()
         text = clean_text(text)
         words = regexp_tokenize(text, pattern = '\s+', gaps = True)
         return [lemmatizer.lemmatize(word) for word in words if (len(word) >= 3)]
+
+
+def getTags(preds_proba, encoder):
+    classes = encoder.classes_
+    if preds_proba.max() >= .75:
+        best = np.where(preds_proba == preds_proba.max())
+        prediction = classes[best]
+        return [prediction[0]]
+    else:
+        best = np.where(preds_proba == preds_proba.max())
+        best_pred = classes[best]
+        preds_proba = np.where(preds_proba==preds_proba.max(), 0, preds_proba)
+        second = np.where(preds_proba== preds_proba.max())
+        second_pred = classes[second]
+        return [best_pred[0], second_pred[0]]
