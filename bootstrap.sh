a5ccb727853186040255aa8886f0345dd662b66a
JeanFraga <fragajean7@gmail.com>
2020-02-03 17:31:57 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,4 @@
+sudo apt install git-all
 sudo apt-get update && sudo apt-get upgrade -y
 
 curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
