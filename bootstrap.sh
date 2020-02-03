04eb35d1a62dbf012fc2d89cf228890e69981c0f
JeanFraga <fragajean7@gmail.com>
2020-02-03 17:21:24 -0500

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+sudo apt-get update && sudo apt-get upgrade -y
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.25.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+git clone --single-branch --branch master https://github.com/Lambda-School-Labs/tagger-ds.git
+cd Flask Test
+
+sudo docker-compose up --build
