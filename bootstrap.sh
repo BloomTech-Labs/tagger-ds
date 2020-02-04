7d0baa554b956d4d887f5869c95ee091f1f0b65c
JeanFraga <fragajean7@gmail.com>
2020-02-04 17:35:00 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ sudo apt-get update && sudo apt-get upgrade -y
 curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-git clone --single-branch --branch master/Jean_EC2test https://github.com/Lambda-School-Labs/tagger-ds.git
+git clone --single-branch --branch Jean_EC2test https://github.com/Lambda-School-Labs/tagger-ds.git
 cd tagger-ds/flask_test
 
 sudo docker-compose up --build
