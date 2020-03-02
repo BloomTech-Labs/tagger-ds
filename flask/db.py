b6626a88100174b7755595451d0c3bebb5a1636c
apathyhill <apathyhill@gmail.com>
2020-03-02 16:00:17 -0500

    Update Predictions
    Former-commit-id: a4f488f077d795ff50395355669e3c458cb5d7e4
    Former-commit-id: 35c827c16f93e3932af98a6b90e1fe96c651f53f

--- flask/db.py
+++ flask/db.py
@@ -5,3 +5,5 @@ DB = SQLAlchemy()
 class User(DB.Model):
     email_address = DB.Column(DB.Text(), nullable=False, primary_key=True)
     pickle_file = DB.Column(DB.LargeBinary(), nullable=False)
+    uids = DB.Column(DB.LargeBinary(), nullable=False)
+    dtype = DB.Column(DB.Text(), nullable=False)
