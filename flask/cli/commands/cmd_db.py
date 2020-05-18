f5c082e8b5226fd702391c1b8d66ba59d7315ee8
Brandon <bsmulas1535@gmail.com>
2020-05-18 16:18:55 -0700

    removes snakeeyes lines

--- flask/cli/commands/cmd_db.py
+++ flask/cli/commands/cmd_db.py
@@ -2,10 +2,7 @@ import click
 
 # from sqlalchemy_utils import database_exists, create_database
 
-# figure out where snakeeeyes comes from
-# 
 from tagger.application import create_app
-# from snakeeyes.extensions import db
 from db import DB, User
 
 # Create an app context for the database connection.
