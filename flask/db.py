0dbb65eda5ece5a77c2528f21aad03eb4f01b4cc
Brandon <bsmulas1535@gmail.com>
2020-05-19 13:28:17 -0700

    removes docker and cli files / folders

--- flask/db.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-DB = SQLAlchemy()
-
-class User(DB.Model):
-    email_address = DB.Column(DB.Text(), nullable=False, primary_key=True)
-    pickle_file = DB.Column(DB.LargeBinary(), nullable=False)
-    uids = DB.Column(DB.LargeBinary(), nullable=False)
-    dtype = DB.Column(DB.Text(), nullable=False)
