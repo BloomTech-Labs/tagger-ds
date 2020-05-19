ad54295e4c2eaa61dae350b404e3d20c6a96527b
Brandon <bsmulas1535@gmail.com>
2020-05-19 16:44:19 -0700

    add config with automatic environment handlling

--- tagger/__init__.py
+++ tagger/__init__.py
@@ -2,8 +2,9 @@ from flask import Flask
 
 def create_app():
     """ Initialize the application """
-    app = Flask(__name__)
-
+    app = Flask(__name__, instance_relative_config=False)
+    app.config.from_object('config.Config')
+    
     with app.app_context():
         # Include routes
         from . import routes
