8f0c89bd7e6a4ed63177124de890057151613b62
Brandon <bsmulas1535@gmail.com>
2020-05-19 13:47:25 -0700

    convert init to application factory syntax

--- tagger/__init__.py
+++ tagger/__init__.py
@@ -1,3 +1,11 @@
-from .application import create_app
+from flask import Flask
 
-APP = create_app()
+def create_app():
+    """ Initialize the application """
+    app = Flask(__name__)
+
+    with app.app_context():
+        # Include routes
+        from . import routes
+
+        return app
