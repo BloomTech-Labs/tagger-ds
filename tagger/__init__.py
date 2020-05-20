372e6bd6b54f7dedcb469d7f0e1aa6a7abb577b6
Brandon <bsmulas1535@gmail.com>
2020-05-19 21:04:22 -0700

    add flask-markdown

--- tagger/__init__.py
+++ tagger/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flaskext.markdown import Markdown
+
+md = Markdown()
 
 def create_app():
     """ Initialize the application """
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object('config.Config')
-    
+
+    # Application plugins
+    md.init_app(app)
+
     with app.app_context():
         # Include routes
         from . import routes
