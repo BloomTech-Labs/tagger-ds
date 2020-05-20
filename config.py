6dc595901e0401ac1ff388b1e194ba224470a8f1
Brandon <bsmulas1535@gmail.com>
2020-05-19 20:11:55 -0700

    add route methods

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ load_dotenv('.env')
 
 class Config:
     """ Base configuration """
+
     SECRET_KEY = os.environ.get('SECRET_KEY')
     FLASK_ENV = os.environ.get('FLASK_ENV')
     DEBUG = os.environ.get('DEBUG')
--- tagger/routes.py
+++ tagger/routes.py
@@ -1,7 +1,14 @@
 from flask import current_app as app
-from flask import request, make_response, jsonify
+from flask import request, make_response, jsonify, redirect, url_for
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
+    return redirect('/api/docs')
+
+@app.route('/api/docs', methods=['GET'])
+def docs():
+    return "This is where the docs will go."
+
+@app.route('/api/sync', methods=['POST'])
+def sync():
     pass
-    
