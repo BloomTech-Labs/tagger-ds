e24757948430b87d7339660c2abafc42bdff5a7c
Brandon <bsmulas1535@gmail.com>
2020-05-28 06:02:48 -0700

    removes load user

--- tagger/credentials.py
+++ tagger/credentials.py
@@ -1,18 +1,6 @@
 from datetime import datetime
+import json
 from google.oauth2.credentials import Credentials
-
-
-def posix_datetime(utc_timestamp):
-    """ Take a UTC Timestamp and return the POSIX datetime tuple """
-
-    posix_date = datetime.utcfromtimestamp(utc_timestamp)
-    return posix_date
-
-def load_user_token(token):
-    """ Loads user token """
-
-    user_token = json.loads(token)
-    return user_token
 
 def construct_creds(info):
     """ Builds credentials from token info """
