d9b28b81b728629c5fff8f7a9ad6e0bdc60106fa
Brandon <bsmulas1535@gmail.com>
2020-05-28 05:35:09 -0700

    adds style fixes

--- tagger/credentials.py
+++ tagger/credentials.py
@@ -4,6 +4,7 @@ from google.oauth2.credentials import Credentials
 
 def posix_datetime(utc_timestamp):
     """ Take a UTC Timestamp and return the POSIX datetime tuple """
+
     posix_date = datetime.utcfromtimestamp(utc_timestamp)
     return posix_date
 
@@ -15,5 +16,6 @@ def load_user_token(token):
 
 def construct_creds(info):
     """ Builds credentials from token info """
+
     creds = Credentials.from_authorized_user_info(info)
     return creds
