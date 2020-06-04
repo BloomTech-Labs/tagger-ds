d1b9411402a7ca6c5385e8cff80eb23638dde4a0
Brandon <bsmulas1535@gmail.com>
2020-06-03 18:00:19 -0700

    make initial set of changes to optimize codebase

--- tagger/message_retriever.py
+++ tagger/message_retriever.py
@@ -1,188 +1,205 @@
-# Module imports / focused on Google API
-import base64
-from bs4 import BeautifulSoup
-import json
+# Core imports
 import re
-import pandas as pd
+import json
+import base64
 from collections import Counter
+# Parsing
+import pandas as pd
+from bs4 import BeautifulSoup
+# Google API
 from googleapiclient.discovery import build
-from gensim.corpora import Dictionary
-from gensim.models.ldamulticore import LdaMulticore
-from gensim.models.coherencemodel import CoherenceModel
-import spacy
+from google.oauth2.credentials import Credentials
+# NLP
 import nltk
-import string
+import spacy
+from gensim.corpora import Dictionary
 from spacy.lang.en.stop_words import STOP_WORDS
-from nltk.corpus import stopwords
-from nltk.stem.wordnet import WordNetLemmatizer
-from flask import jsonify
+from gensim.models.ldamulticore import LdaMulticore
 
-# If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+def preprocess_string(text):
+    """Process a string for purpose of tagging.
 
-previous_email_pull = '172317ca92683d33'
+    Args:
+        text (string): The email parsed email body.
 
-def user_emails(creds):
-    """ Pulls user emails """
+    Returns:
+        text (string): The processed string.
+    """
+    text = text.strip().lower()
+    text = text.replace(
+        '[^a-zA-Z\s]', ''
+    ).replace(
+        '\s+', ' '
+    )
+    return text
 
-    service = build('gmail', 'v1', credentials=creds)
+def tokenize_string(text, nlp):
+    """Generate tokens for a given body of text.
 
-    # Call the Gmail API
-    emails = service.users().messages().list(userId='me').execute()
+    Args:
+        text (string): Processed body of text
+        nlp: Spacy en_core_web_sm model.
 
-    return emails
-
-def recent_id(emails):
-    # Find most recent message
-    recent_msg_id = emails['messages'][0]['id']
-    return recent_msg_id
-
-
-def tag_recent(recent_msg_id, creds):
-    """ Generates tags on the most recent email """
-
-    # Build service
-    service = build('gmail', 'v1', credentials=creds)
-
-    # PRINT 1
-    print("Getting most recent message...")
-
-    # Call message content
-    message_content = service.users().messages().get(
-        userId='me', id=recent_msg_id).execute()
-
-    # Call message body
-    message_payload = message_content['payload']
-    if "parts" in message_payload:
-        message_body = message_content['payload']['parts']
-        message_body_dict = dict(message_body[0])
-    else:
-        message_body = message_content['payload']['body']['data']
-        message_body_dict = dict(message_body[0])
-
-    # PRINT 2
-    print("Most recent message obtained. Decoding...")
-
-    # Decode base64 encoding
-    decode = message_body_dict['body']['data']
-    decodedContents = base64.urlsafe_b64decode(decode.encode('utf-8'))
-
-    # Clean the text
-    text = BeautifulSoup(decodedContents, 'html.parser')
-    text = str(text.text)
-    text = re.sub(r"http\S+", "", text)
-    text = [text]
-
-    # PRINT 3
-    print("Decoded message body. Loading spacy small model...")
-
-    # Load spacy model
-    nlp = spacy.load('en_core_web_sm')
-
-    # PRINT 4
-    print("Spacy model loaded. Creating stop words list...")
-
-    my_stop_words = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves',
-                 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours',
-                 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she',
-                 "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself',
-                 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which',
-                 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am',
-                 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has',
-                 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the',
-                 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while',
-                 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between',
-                 'into', 'through', 'during', 'before', 'after', 'above', 'below',
-                 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under',
-                 'again', 'further', 'then', 'once', 'here', 'there', 'when',
-                 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few',
-                 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not',
-                 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't',
-                 'can', 'will', 'just', 'don', "don't", 'should', "should've",
-                 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren',
-                 "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn',
-                 "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven',
-                 "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't",
-                 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't",
-                 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren',
-                 "weren't", 'won', "won't", 'wouldn', "wouldn't", "unsubscribe",
-                 "st"]
-
-    STOP_WORDS = nlp.Defaults.stop_words.union(my_stop_words)
-    # stop = stopwords.words('english')
-
-    # PRINT 4
-    print("Generating email dataframe...")
-
-    # Shape text & tokenize
-    df = pd.DataFrame(text, columns = ['email_body'])
-
-    #remove whitespace
-    df['email_body'] = df['email_body'].str.strip().str.lower()
-
-    #Start with date
-    df['email_body'].str.match('\d?\d/\d?\d/\d{4}').all()
-
-    #Replace all non-overlapping matches
-    df['email_body'] = df['email_body'].str.replace('[^a-zA-Z\s]', '').str.replace('\s+', ' ')
-
-    # PRINT 5
-    print("Tokenizing data...")
-
-    # Generate NLP model
-    tokens = []
-    for doc in nlp.pipe(df['email_body'], batch_size=500):
+    Returns:
+        tokens (list): List of tokens
+    """
+    tokens = list()
+    for doc in nlp.pipe(text, batch_size=500):
         doc_tokens = []
         for token in doc:
             if (token.is_stop is False) & (token.is_punct is False):
                 doc_tokens.append(token.lemma_.lower())
         tokens.append(doc_tokens)
-    df['tokens'] = tokens
+    return tokens
 
-    # PRINT 6
-    print("Modelling token data for Topics with LDA...")
+def generate_tags(tokens:list) -> list:
+    """Perform LDA Topic Modelling to aquire tags.
 
-    # Topic Modelling
-    id2word = Dictionary(df['tokens'])
-    print("Created Dictionary")
-    corpus = [id2word.doc2bow(d) for d in df['tokens']]
-    print("created Corpus")
-    model = LdaMulticore(corpus=corpus,
-                         id2word=id2word,
-                         random_state=42,
-                         num_topics=10,
-                         passes=1,
-                         workers=1)
-    print("Created LDA Model")
+    Args:
+        tokens (list): List of tokens
 
-    # PRINT 7
-    print("Building tag list...")
-
-    # Generate topics from model
+    Returns:
+        tags_list (list) List of appropriate tags for
+        given tokens.
+    """
+    id2word = Dictionary(tokens)
+    corpus = [id2word.doc2bow(d) for d in tokens]
+    model = LdaMulticore(
+        corpus=corpus,
+        id2word=id2word,
+        random_state=42,
+        num_topics=10,
+        passes=2,
+        workers=1
+    )
     words = [re.findall(r'"([^"]*)"', t[1]) for t in model.print_topics()]
-
-    # Count instances of each word seeking weight of dupes
     wordcount = Counter(words[0] + words[1] + words[2] + words[3] + words[4])
-
-    # Generate dataframe of results, drop those found less than twice, sort descending
     tags = pd.DataFrame.from_dict(wordcount, orient='index', columns=['number'])
-    tags.drop(tags[tags.number <= 1].index, inplace=True)
-    tags.sort_values(by=['number'], ascending=False, inplace=True)
-    tags['length'] = range(len(tags))
-    tags = tags.T
-    tags_list = tags.columns
-    email_tags = {'tokens':[word for word in tags_list]}
-    email = {'email':decode, 'tokens':[word for word in tags_list]}
+    tags = tags.drop(tags[tags['number'] <= 1].index)
+    tags = tags.sort_values(by=['number'], ascending=False).T
+    tags_list = [word for word in tags.columns]
 
-    # PRINT 8
-    print("Complete, returning json...")
+    return tags_list
 
-    # Return sorted smart tag list
-    return jsonify(email)
 
-# # Recent messages list
-# message_tally = []
-# for _ in range(len(message['messages'])):
-#     message_tally.append(message['messages'][_]['id'])
-#     if message['messages'][_]['id'] == previous_email_pull:
-#         break
+def build_service(info: dict):
+    """Function to build the Resource object.
+
+    Args:
+        info (dict): Token containing valid user auth
+        refresh token, client_id, and client_secret
+
+    Returns:
+        service (googleapiclient.discovery.Resource)
+
+    Example:
+        >>> info = {
+        ...     refresh_token: "my_refresh_token_hash",
+        ...     client_id: "aW82nG-so_client_id",
+        ...     client_secret: "super_secret_hash"
+        ... }
+        >>> service = build_service(info)
+        >>> type(service)
+        <class 'googleapiclient.discovery.Resource'>
+    """
+    creds = Credentials.from_authorized_user_info(info)
+    service = build('gmail', 'v1', credentials=creds)
+    return service
+
+def user_emails(service, recent_id=None) -> list:
+    """Retrive user emails.
+
+    Args:
+        service (googleapiclient.discovery.Resource):
+        Resource object that will be used to obtain the
+        list of emails.
+
+        recent_id (string): The most recent id to begin pulling
+        emails from. None by default, which will pull all emails.
+
+    Returns:
+        emails (list): A list of dictionaries that contain
+        the user's emails.
+    """
+    email_list = list()
+    # Grab first page of emails ids
+    emails = service.users().messages().list(
+            userId='me'
+        ).execute()
+    email_list = [x['id'] for x in emails['messages']]
+    while "nextPageToken" in emails.keys():
+        if recent_id in email_list:
+            break
+        else:
+            emails = service.users().messages().list(
+                    userId='me', pageToken=emails['nextPageToken']
+                ).execute()
+            email_list = email_list + [x['id'] for x in emails['messages']]
+
+    if recent_id not None:
+        idx = email_list.index(recent_id)
+        email_list = email_list[:idx]
+    return email_list
+
+def generate_emails(service, id_list):
+    """Generator object yielding individual emails.
+
+    Args:
+        service (googleapiclient.discovery.Resource):
+        Resource Object used to query the Gmail API
+        for an authorized user.
+
+        id_list (list): List of email ids that will
+        be passed into the api queries.
+
+    Returns:
+        Generator that yields each individual email.
+    """
+    generator = (
+            service.users().messages().get(
+                userId='me', id=x
+                ).execute()
+            for x in id_list
+            )
+    return generator
+
+def generate_tagged_emails(service, email_gen):
+    """Tag recent emails
+
+    Args:
+        service (googleapiclient.discovery.Resource):
+        Resource to query the GMail API.
+
+        email_gen (generator): Generator that supplies
+        each email for the user.
+
+    Yields:
+        email (dict): Dictionary containing the email data.
+    """
+
+    nlp = space.load("en_core_web_sm")
+    stopwords = STOP_WORDS # Stop words
+
+    for email in email_gen:
+
+        # Begin tagging logic
+        message_payload = message_content['payload']
+        if 'parts' in message_payload:
+            message_body = message_payload['parts'][1]['body']['data']
+        else:
+            message_body = message_payload['body']['data']
+
+        message_text = base64.urlsafe_b64decode(message_body.encode('utf-8'))
+        text = re.sub(
+            r"https\S+", "", str(
+                BeautifulSoup(message_text, 'html.parser').text
+            )
+        )
+
+        text = preprocess_string(text)
+        tokens = tokenize_string(text, nlp)
+        tags_list = generate_tags(tokens)
+
+        email['smartTags'] = [word for word in tags_list]
+
+        yield email
